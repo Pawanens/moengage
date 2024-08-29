8e05d912df5597ab9e11c2e757a46f04239458a7
kpawan9568@gmail.com <kpawan9568@gmail.com>
2024-08-29 17:29:45 +0530

    new changes

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.use(express.json());
 const port = 3000;
 app.post("/data", (req, res)=>{
     const {Name , Email} =req.body
-    console.log("Name" , Name)
-    console.log("Email" , Email)
-    res.send(Name)
+    console.log("checkdata" , req.body)
+    // console.log("Name" , Name)
+    // console.log("Email" , Email)
+    // res.send(Name)
 })
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
