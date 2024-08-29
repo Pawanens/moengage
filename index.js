637ce3550188cb39afb33a70153e7d877ba48d91
kpawan9568@gmail.com <kpawan9568@gmail.com>
2024-08-29 16:35:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+
+const port = 3000;
+app.post("/data", (req, res)=>{
+    const {Name , Email} =req.body
+    console.log("Name" , Name)
+    console.log("Email" , Email)
+    res.send(Name)
+})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+
